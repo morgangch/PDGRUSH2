76837964fc6cede0acbaed6da1ac3118545097ef
Morgan <morgan.guichard@epitech.eu>
2025-01-22 16:30:46 +0100

    TEMP comment main code

--- main_test/ex06.c
+++ main_test/ex06.c
@@ -9,20 +9,22 @@
 
 int main(void)
 {
-    Object *list = new(List, 10, Int, 0);
-    Object *it = begin(list);
-    Object *it_end = end(list);
-
-    printf("list size: %zu\n", len(list));
-    setitem(list, 5, 42);
-    setitem(list, 6, 84);
-    while (lt(it, it_end))
-    {
-        printf("%s\n", str(getval(it)));
-        incr(it);
-    }
-    delete(it);
-    delete(it_end);
-    delete(list);
     return 0;
 }
+//     Object *list = new(List, 10, Int, 0);
+//     Object *it = begin(list);
+//     Object *it_end = end(list);
+
+//     printf("list size: %zu\n", len(list));
+//     setitem(list, 5, 42);
+//     setitem(list, 6, 84);
+//     while (lt(it, it_end))
+//     {
+//         printf("%s\n", str(getval(it)));
+//         incr(it);
+//     }
+//     delete(it);
+//     delete(it_end);
+//     delete(list);
+//     return 0;
+// }
