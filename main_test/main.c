1f0d8088f8a44e49865e8577114b242407381db5
Noa González <noa.gonzalez-gomez@epitech.eu>
2025-01-22 12:05:05 +0100

    [DEL] Deleted main.

--- main_test/main.c
+++ main_test/main.c
@@ -6,16 +6,3 @@
 */
 
 #include "../include/my.h"
-
-int main(void)
-{
-    Object  *point = new(Point, 42, -42);
-    Object  *vertex = new(Vertex, 0, 1, 2);
-
-    printf("point = %s\n", str(point));
-    printf("vertex = %s\n", str(vertex));
-
-    delete(point);
-    delete(vertex);
-    return (0);
-}
